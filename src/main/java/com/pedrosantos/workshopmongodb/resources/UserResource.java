fa511f08bfc96accbb4f45864e3e0cf99df0b4c9
Pedro Santos <phgomes03122004@gmail.com>
2025-08-04 16:53:30 -0300

    Entity User and REST working

---
+++ src/main/java/com/pedrosantos/workshopmongodb/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.pedrosantos.workshopmongodb.resources;
+
+import com.pedrosantos.workshopmongodb.domain.User;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+}
