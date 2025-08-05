a81915f865f04eb9ea16d05417bcb5aec07d9692
Pedro Santos <phgomes03122004@gmail.com>
2025-08-04 21:23:14 -0300

    Endpoint to retrieve a user's posts

--- src/main/java/com/pedrosantos/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/pedrosantos/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.pedrosantos.workshopmongodb.resources;
 
+import com.pedrosantos.workshopmongodb.domain.Post;
 import com.pedrosantos.workshopmongodb.domain.User;
 import com.pedrosantos.workshopmongodb.dto.UserDTO;
 import com.pedrosantos.workshopmongodb.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
