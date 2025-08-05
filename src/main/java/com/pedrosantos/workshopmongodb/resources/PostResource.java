a190696ad229830cb3999bbba22858a6665ed39e
Pedro Santos <phgomes03122004@gmail.com>
2025-08-04 21:58:14 -0300

    Simple queries with query methods

--- src/main/java/com/pedrosantos/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/pedrosantos/workshopmongodb/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.pedrosantos.workshopmongodb.resources;
 
 import com.pedrosantos.workshopmongodb.domain.Post;
+import com.pedrosantos.workshopmongodb.resources.util.URL;
 import com.pedrosantos.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -17,5 +20,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
